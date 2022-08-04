045267a10ab0005ac56e53e9a572ad0c9f419f21
Ilya57061 <81263807+Ilya57061@users.noreply.github.com>
2022-08-05 00:09:48 +0300

    Update TaxiContext.cs

--- Taxi/Taxi.Model/Database/TaxiContext.cs
+++ Taxi/Taxi.Model/Database/TaxiContext.cs
@@ -10,13 +10,7 @@ namespace Taxi.Model.Database
             
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-            modelBuilder.Entity<Category>().HasData(new Category { Id=1, Name="Эконом", Description="nn", Price=1});
-            modelBuilder.Entity<Driver>().HasData(new Driver { Id = 1, DrivingExperience=3, Name = "Александр Гержа"});
-            modelBuilder.Entity<Car>().HasData(new Car { Id = 1, CategoryId=1, DriverId=1, Model="VWPolo", Number="3232" });
-        }
+      
         public DbSet<Car> Cars { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Driver> Drivers { get; set; }
